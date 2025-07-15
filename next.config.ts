f79bc26418ed6b5c70fed54d537d8f02864fff93
Stefan Milenkovic <stefanmilenkovicbusiness@gmail.com>
2025-07-15 11:47:41 +0200

    add ppr to config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
+    ppr: "incremental",
     viewTransition: true,
     reactCompiler: {
       compilationMode: "annotation",
